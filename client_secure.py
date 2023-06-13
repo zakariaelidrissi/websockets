819cb3aa57cee8649ac2551502df72800afa6a30
Zakaria EL Iidrissi <61559275+zakariaelidrissi@users.noreply.github.com>
2023-06-13 13:08:34 +0100

    update

--- client_secure.py
+++ client_secure.py
@@ -1,15 +1,20 @@
 import asyncio
 import pathlib
-import ssl
-import websockets
+import ssl, cv2
+import websockets, base64
 
-ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-localhost_pem = pathlib.Path(__file__).with_name("localhost.pem")
-ssl_context.load_verify_locations(localhost_pem)
+# IMAGE_PATH = "images/left_01062023_144237.jpg" 
+# IMAGE_PATH = "images/stop_01062023_144448.jpg"
+IMAGE_PATH = "images/forward_01062023_145103.jpg"
+
+# ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+# localhost_pem = pathlib.Path(__file__).with_name("localhost.pem")
+# ssl_context.load_verify_locations(localhost_pem)
+uri = "ws://192.168.3.54:8765"
 
 async def hello():
-    uri = "wss://localhost:8765"
-    async with websockets.connect(uri, ssl=ssl_context) as websocket:
+    # uri = "wss://localhost:8765"
+    async with websockets.connect(uri) as websocket:
         name = input("What's your name? ")
 
         await websocket.send(name)
@@ -18,5 +23,23 @@ async def hello():
         greeting = await websocket.recv()
         print(f"<<< {greeting}")
 
+# send image to the server
+async def predict():
+    global uri
+    # Read the image file    
+    image = cv2.imread(IMAGE_PATH)
+    retval, buffer = cv2.imencode('.jpg', image)
+    # Encode the image data to base64
+    base64_data = base64.b64encode(buffer).decode("utf-8")
+    
+    # Connect to the WebSocket server
+    async with websockets.connect(uri) as websocket:
+        # Send the encoded image data
+        await websocket.send(base64_data)
+
+        pred_class_name = await websocket.recv()
+        print(f"<<< {pred_class_name}")
+
+
 if __name__ == "__main__":
-    asyncio.run(hello())
+    asyncio.run(predict())
--- server_secure.py
+++ server_secure.py
@@ -2,6 +2,12 @@ import asyncio
 import pathlib
 import ssl
 import websockets
+import base64, cv2, numpy as np
+from ultralytics import YOLO
+
+MODEL_PATH = 'model/best.pt'
+
+model = YOLO(MODEL_PATH)  # load a custom model
 
 async def hello(websocket):
     name = await websocket.recv()
@@ -12,12 +18,32 @@ async def hello(websocket):
     await websocket.send(greeting)
     print(f">>> {greeting}")
 
-ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-localhost_pem = pathlib.Path(__file__).with_name("localhost.pem")
-ssl_context.load_cert_chain(localhost_pem)
+# ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+# localhost_pem = pathlib.Path(__file__).with_name("localhost.pem")
+# ssl_context.load_cert_chain(localhost_pem)
+
+
+async def receive_image(websocket):
+    async for message in websocket:
+        # Decode the base64 image data
+        image_data = base64.b64decode(message)        
+        nparr = np.frombuffer(image_data, np.uint8)
+        
+        # Do further processing with the image as needed
+        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+        # predict an image
+        results = model(img)
+        names_dict = results[0].names
+        probs = results[0].probs.tolist()
+        pred_class_name = names_dict[np.argmax(probs)]
+
+        await websocket.send(pred_class_name)
+        print(f">>> {pred_class_name}")
+
 
 async def main():
-    async with websockets.serve(hello, "localhost", 8765, ssl=ssl_context):
+    async with websockets.serve(receive_image, "192.168.3.54", 8765):
         await asyncio.Future()  # run forever
 
 if __name__ == "__main__":
