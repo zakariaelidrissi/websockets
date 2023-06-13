fd21c098e4bf03aa0d78a96cd82846c759f9fc6c
Zakaria EL Iidrissi <61559275+zakariaelidrissi@users.noreply.github.com>
2023-06-13 11:29:21 +0100

    Initial implementation.

---
+++ server.py
@@ -0,0 +1,22 @@
+import asyncio
+import websockets
+
+async def hello(websocket):
+    name = await websocket.recv()
+    print(f"<<< {name}")
+
+    greeting = f"Hello {name}!"
+
+    await websocket.send(greeting)
+    print(f">>> {greeting}")
+
+async def main():
+    async with websockets.serve(hello, "localhost", 8765):
+        await asyncio.Future()  # run forever
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+
+# run the server with watchmedo - mode : auto-restart
+# watchmedo auto-restart --pattern "*.py" --recursive --signal SIGTERM python server.py
---
+++ server_secure.py
@@ -0,0 +1,28 @@
+import asyncio
+import pathlib
+import ssl
+import websockets
+
+async def hello(websocket):
+    name = await websocket.recv()
+    print(f"<<< {name}")
+
+    greeting = f"Hello {name}!"
+
+    await websocket.send(greeting)
+    print(f">>> {greeting}")
+
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+localhost_pem = pathlib.Path(__file__).with_name("localhost.pem")
+ssl_context.load_cert_chain(localhost_pem)
+
+async def main():
+    async with websockets.serve(hello, "localhost", 8765, ssl=ssl_context):
+        await asyncio.Future()  # run forever
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+
+# run the server with watchmedo - mode : auto-restart
+# watchmedo auto-restart --pattern "*.py" --recursive --signal SIGTERM python server.py
